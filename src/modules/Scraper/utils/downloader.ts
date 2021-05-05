4e31a6b0fd55ef1c8902a2ad747ce760556eee69
Martin Ratinaud <martinratinaud@gmail.com>
2021-05-05 16:19:42 +0400

    feat: prevent cookie banners to load by using code from a popular web extension

--- src/modules/Scraper/utils/downloader.ts
+++ src/modules/Scraper/utils/downloader.ts
@@ -1,33 +1,52 @@
+import {
+  getHostlevels,
+  interceptCookieUrls,
+  removeCookieBanners,
+} from '../i-dont-care-about-cookies';
+
+import { URL } from 'url';
+import debug from 'debug';
 import fse from 'fs-extra';
 import puppeteer from 'puppeteer';
 
-export const downloadUrl = async (url: string, { folderPath }: { folderPath: string }) => {
-  const browser = await puppeteer.launch({
-    // @ts-ignore
-    executablePath: process.env.CHROME_BIN || null,
-    headless: true,
-    args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage'],
-  });
-  const page = await browser.newPage();
-  console.log(`Create dir ${folderPath}`);
+const logDebug = debug('ota.org:debug');
 
+export const downloadUrl = async (url: string, { folderPath }: { folderPath: string }) => {
   fse.ensureDirSync(folderPath);
 
-  await page.goto(url, {
-    waitUntil: 'networkidle0',
+  const browser = await puppeteer.launch({
+    executablePath: process.env.CHROME_BIN,
+    headless: true,
+    ignoreDefaultArgs: ['--disable-extensions', '--enable-automation'],
+    args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
   });
+  const page = await browser.newPage();
+  page.on('console', (consoleObj: any) => logDebug('>> in page', consoleObj.text()));
 
+  const { hostname } = new URL(url);
+  const hostLevels = getHostlevels(hostname);
+
+  // Intercept not wanted requests
   await page.setRequestInterception(true);
   page.on('request', (req) => {
-    if (req.isNavigationRequest() && req.frame() === page.mainFrame() && req.url() !== url) {
+    const reqUrl = req.url();
+    if (
+      interceptCookieUrls(reqUrl, hostLevels) ||
+      (req.isNavigationRequest() && req.frame() === page.mainFrame() && reqUrl !== url)
+    ) {
       req.abort('aborted');
     } else {
       req.continue();
     }
   });
 
+  await page.goto(url, { waitUntil: 'networkidle0' });
+
+  await removeCookieBanners(page, hostname);
+
   const html = await page.content();
   fse.writeFileSync(`${folderPath}/index.html`, html.replace(/<script.*?>.*?<\/script>/gim, ''));
 
+  browser.close();
   return folderPath;
 };
