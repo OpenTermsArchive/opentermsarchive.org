5ca1afc68bbd2b197b2eeed4835a7505bcf7858f
Martin Ratinaud <martinratinaud@gmail.com>
2021-05-11 15:53:51 +0400

    wip: add terms of service page

---
+++ src/modules/Common/pages/terms-of-service.tsx
@@ -0,0 +1,12 @@
+import Layout from '../containers/Layout';
+
+const TermsOfServicePage = () => {
+  return (
+    <Layout>
+      <h1>Terms of service</h1>
+      <h2>dede</h2>
+    </Layout>
+  );
+};
+
+export default TermsOfServicePage;
---
+++ src/pages/terms-of-service.tsx
@@ -0,0 +1,1 @@
+export { default } from 'modules/Common/pages/terms-of-service';
