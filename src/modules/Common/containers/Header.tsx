77e86548c3030f69418231b57ea0ccf63bc18e18
Clément Biron“ <clement.biron@gmail.com>
2021-05-19 15:17:22 +0200

    Add toggle

--- src/modules/Common/containers/Header.tsx
+++ src/modules/Common/containers/Header.tsx
@@ -1,3 +1,6 @@
+import { useLockBodyScroll, useToggle } from 'react-use';
+
+import { FiX } from 'react-icons/fi';
 import Link from 'next/link';
 import Logo from 'modules/Common/components/Logo';
 import React from 'react';
@@ -9,14 +12,25 @@ type HeaderProps = {
 } & React.HTMLAttributes<HTMLDivElement>;
 
 const Header: React.FC<HeaderProps> = ({ children, className, ...props }) => {
+  const [open, toggleExtended] = useToggle(false);
+  useLockBodyScroll(open);
+
   return (
-    <header className={s.header}>
+    <header className={classNames(s.header, { [s.header__isOpen]: open })}>
       <div className={s.header_logo}>
         <Link href="/">
           <Logo />
         </Link>
       </div>
       <div className={classNames(s.header_menus)}>{children}</div>
+
+      <button type="button" className={classNames(s.header_openLink)} onClick={toggleExtended}>
+        Menu
+      </button>
+      <button type="button" className={classNames(s.header_closeLink)} onClick={toggleExtended}>
+        Close
+        <FiX color="#fefffd" />
+      </button>
     </header>
   );
 };
