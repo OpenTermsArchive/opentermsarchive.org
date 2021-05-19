f0489dcd15018a319645e11c9b1f10b3042b980d
Clément Biron“ <clement.biron@gmail.com>
2021-05-19 11:53:55 +0200

    Header component WIP

--- src/modules/Common/containers/Header.tsx
+++ src/modules/Common/containers/Header.tsx
@@ -1,14 +1,22 @@
+import Link from 'next/link';
+import Logo from 'modules/Common/components/Logo';
 import React from 'react';
-import LanguageSwitcher from 'modules/I18n/components/LanguageSwitcher';
+import classNames from 'classnames';
 import s from './Header.module.css';
-import Container from './Container';
 
-const Header = () => {
+type HeaderProps = {
+  className?: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const Header: React.FC<HeaderProps> = ({ children, className, ...props }) => {
   return (
     <header className={s.header}>
-      <Container className={s.header_content}>
-        Header <LanguageSwitcher />{' '}
-      </Container>
+      <div className={s.header_logo}>
+        <Link href="/">
+          <Logo />
+        </Link>
+      </div>
+      <div className={classNames(s.header_menus)}>{children}</div>
     </header>
   );
 };
--- src/modules/Common/containers/Layout.tsx
+++ src/modules/Common/containers/Layout.tsx
@@ -2,11 +2,15 @@ import React, { ReactNode } from 'react';
 
 import { Analytics } from 'modules/Analytics';
 import Container from './Container';
+import { FiGithub } from 'react-icons/fi';
 import Footer from './Footer';
 import FooterMenu from '../components/FooterMenu';
 import Head from 'next/head';
 import Header from './Header';
+import HeaderMenu from '../components/HeaderMenu';
+import LanguageSwitcher from 'modules/I18n/components/LanguageSwitcher';
 import Link from 'next/link';
+import classNames from 'classnames';
 
 type Props = {
   children?: ReactNode;
@@ -21,7 +25,39 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Analytics />
-    <Header />
+
+    {/* Header */}
+    <Container paddingY={false} paddingX={false} layout="fluid">
+      <Container gridCols="12" gridGutters="11" flex={true} paddingX={false} paddingY={false}>
+        <Header>
+          <HeaderMenu>
+            <ul>
+              <li>
+                <Link href="/#how">How ?</Link>
+              </li>
+              <li>
+                <Link href="/#values">Values</Link>
+              </li>
+              <li>
+                <Link href="/#conntribute">Contribute</Link>
+              </li>
+              <li>
+                <Link href="/#built-with">Built with</Link>
+              </li>
+            </ul>
+          </HeaderMenu>
+          <HeaderMenu>
+            <LanguageSwitcher />
+            <Link href="https://github.com/ambanum/OpenTermsArchive/">
+              <a className={classNames('icon_github_circle')}>
+                <FiGithub color="#fefffd" />
+              </a>
+            </Link>
+          </HeaderMenu>
+        </Header>
+      </Container>
+    </Container>
+
     {children}
 
     {/* Footer */}
