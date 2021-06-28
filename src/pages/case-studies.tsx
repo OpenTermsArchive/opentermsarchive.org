640b591affe500fbbc6349fcd568bdab53966214
Clément Biron <clement.biron@gmail.com>
2021-06-28 16:58:00 +0200

    feat: use hero in case studies page

--- src/pages/case-studies.tsx
+++ src/pages/case-studies.tsx
@@ -2,8 +2,8 @@ import { WithI18nResult, withI18n } from 'modules/I18n';
 
 import Button from 'modules/Common/components/Button';
 import Container from 'modules/Common/containers/Container';
+import Hero from 'modules/Common/components/Hero';
 import Layout from 'modules/Common/containers/Layout';
-import Link from 'next/link';
 import { MDXRemote } from 'next-mdx-remote';
 import React from 'react';
 import TextContent from 'modules/Common/components/TextContent';
@@ -13,9 +13,20 @@ export default function PrivacyPolicyPage({ mdxContent }: WithI18nResult) {
   const { t } = useTranslation('common');
   return (
     <Layout title={t('common:case-studies.seo.title', 'Case Studies')}>
+      <Container layout="wide" backgroundImage="/images/bg1.jpg" dark={true} paddingY={false}>
+        <Container gridCols="12" gridGutters="11" flex={true} paddingX={false}>
+          <Hero
+            title={t('common:case-studies.hero.title', 'Case Studies')}
+            subtitle={t(
+              'common:case-studies.hero.subtitle',
+              'So far, Open Terms Archive taught us a lot.'
+            )}
+          ></Hero>
+        </Container>
+      </Container>
       <Container gridCols="10" gridGutters="9" paddingX={false}>
         <TextContent>
-          <MDXRemote {...(mdxContent as any)} components={{ Button: Button, Link: Link }} />
+          <MDXRemote {...(mdxContent as any)} components={{ Button: Button }} />
         </TextContent>
       </Container>
     </Layout>
