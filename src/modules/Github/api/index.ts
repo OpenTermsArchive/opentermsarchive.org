8f611852c5bbc42fedf562fadf72e318a3eb19f8
Clément Biron <clement.biron@gmail.com>
2021-06-30 17:36:15 +0200

    fix: take the right kind of documents

--- src/modules/Github/api/index.ts
+++ src/modules/Github/api/index.ts
@@ -1,12 +1,12 @@
 const DOCUMENT_TYPES_URL =
-  'https://raw.githubusercontent.com/ambanum/OpenTermsArchive/master/scripts/rewrite/renamer/rules/documentTypes.json';
+  'https://raw.githubusercontent.com/ambanum/OpenTermsArchive/master/src/app/types.json';
 
 import axios from 'axios';
 
 export const getDocumentTypes: any = async () => {
   try {
     const { data: documentTypes } = await axios.get(DOCUMENT_TYPES_URL);
-    return [...new Set(Object.values(documentTypes))].sort();
+    return [...new Set(Object.keys(documentTypes))].sort();
   } catch (e) {
     console.error(e);
     return [];
