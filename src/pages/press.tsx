f982666906f3fd2e32176ed9260dd25bf3edd414
Clément Biron <clement.biron@gmail.com>
2021-07-19 16:04:47 +0200

    feat: add get in touch section in press page with downloadable press kit

--- src/pages/press.tsx
+++ src/pages/press.tsx
@@ -2,9 +2,12 @@ import { WithI18nResult, withI18n } from 'modules/I18n';
 
 import Article from 'modules/Common/components/Article';
 import Button from 'modules/Common/components/Button';
+import Column from 'modules/Common/components/Column';
 import Container from 'modules/Common/containers/Container';
+import { FiArrowRightCircle } from 'react-icons/fi';
 import Hero from 'modules/Common/components/Hero';
 import Layout from 'modules/Common/containers/Layout';
+import Link from 'next/link';
 import { MDXRemote } from 'next-mdx-remote';
 import React from 'react';
 import TextContent from 'modules/Common/components/TextContent';
@@ -29,6 +32,33 @@ export default function PressPage({ mdxContent }: WithI18nResult) {
             <MDXRemote {...(mdxContent as any)} components={{ Button: Button }} />
           </TextContent>
         </Article>
+      </Container>
+      <Container layout="wide" backgroundImage="/images/bg2.jpg" paddingY={false}>
+        <Container gridCols="12" gridGutters="11" flex={true} paddingX={false} dark={true}>
+          <Column
+            width={100}
+            title={t('common:press.getintouch.title', 'Would you like to get in touch with us?')}
+            subtitle={t('common:press.getintouch.subtitle', 'You are journalist')}
+            className="tcenter"
+          >
+            <TextContent>
+              <p>
+                {t('common:press.getintouch.p1', 'Contact us by email:')}{' '}
+                <Link href="mailto:contact@opentermsarchive.org">
+                  <a className="a__darked">contact@opentermsarchive.org</a>
+                </Link>
+              </p>
+              <p>
+                {t('common:press.getintouch.p2', 'Be free to')}{' '}
+                <Link href="/download/press-kit/20210719-open-terms-archive-press-kit.zip">
+                  <a download className="a__darked">
+                    {t('common:press.getintouch.presskitlink.label', 'download the press kit')}
+                  </a>
+                </Link>
+              </p>
+            </TextContent>
+          </Column>
+        </Container>
       </Container>
     </Layout>
   );
