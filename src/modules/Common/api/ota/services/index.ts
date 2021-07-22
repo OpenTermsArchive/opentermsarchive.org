b62a016fcdf41088abea1314ab057a20da4a8209
Martin Ratinaud <martinratinaud@gmail.com>
2021-07-22 10:47:55 +0200

    chore: use official ota.org api url

--- src/modules/Common/api/ota/services/index.ts
+++ src/modules/Common/api/ota/services/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getServices = async () => {
   const { data } = await axios(
-    'https://disinfo.quaidorsay.fr/api/open-terms-archive/list_services/v1/?multiple_versions_only=false'
+    'https://opentermsarchive.org/api/open-terms-archive/list_services/v1/?multiple_versions_only=false'
   );
 
   return data;
