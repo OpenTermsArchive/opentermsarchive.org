b31b77a1b3501032e57424c33b854a548cc33592
Martin Ratinaud <martinratinaud@gmail.com>
2021-08-02 12:07:49 +0200

    feat: put dma actors at the top of the select for easier selection of main services #21

--- src/modules/OTA-api/data-components/SelectService.tsx
+++ src/modules/OTA-api/data-components/SelectService.tsx
@@ -12,93 +12,129 @@ type SelectServiceProps = {
   defaultServices: any;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-const SelectService: React.FC<SelectServiceProps> = ({
-  serviceProps,
-  documentTypeProps,
-  service: selectedService,
-  documentType: selectedDocumentType,
-  defaultServices,
-}) => {
-  const { t } = useTranslation('common');
-  const { data } = useSWR('/api/ota/services', {
-    initialData: defaultServices,
-    revalidateOnMount: true,
-  });
+// https://github.com/ambanum/OpenTermsArchive.org/issues/21
+const dmaActors = [
+  'Amazon.com',
+  'Apple',
+  'Facebook',
+  'Google',
+  'Gmail',
+  'Instagram',
+  'Spotify',
+  'TikTok',
+  'WhatsApp',
+  'YouTube',
+];
 
-  const services = data
-    ? // sort services with insensitive case
-      Object.keys(data).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }))
-    : [];
-  const documentTypes: string[] = data ? data[selectedService] || [] : [];
+const SelectService: React.FC<SelectServiceProps> = React.memo(
+  ({
+    serviceProps,
+    documentTypeProps,
+    service: selectedService,
+    documentType: selectedDocumentType,
+    defaultServices,
+  }) => {
+    const { t } = useTranslation('common');
+    const { data } = useSWR('/api/ota/services', {
+      initialData: defaultServices,
+      revalidateOnMount: true,
+    });
 
-  const loading = !data;
+    const services = data
+      ? // sort services with insensitive case
+        Object.keys(data).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }))
+      : [];
+    const documentTypes: string[] = data ? data[selectedService] || [] : [];
 
-  return (
-    <>
-      <div className={classNames('formfield')}>
-        <label htmlFor="services">
-          {t('common:subscribe_form.fields.service.label', 'Select a service')}
-        </label>
-        <div className={classNames('select')}>
-          {
-            // This is done in order for default values to be selected correctly
-            // if the options are not present at init, it will never be selected
-            loading ? (
-              <select key="select_service_disabled" disabled />
-            ) : (
-              <select
-                key="select_service"
-                id="services"
-                defaultValue={selectedService}
-                {...serviceProps}
-              >
-                <option value="">
-                  {t('common:subscribe_form.fields.service.default', 'Select...')}
-                </option>
-                {services.map((service) => (
-                  <option key={service} value={service}>
-                    {service}
+    const loading = !data;
+
+    const dmaServices = services.filter((service) => dmaActors.includes(service));
+    const otherServices = services.filter((service) => !dmaActors.includes(service));
+
+    return (
+      <>
+        <div className={classNames('formfield')}>
+          <label htmlFor="services">
+            {t('common:subscribe_form.fields.service.label', 'Select a service')}
+          </label>
+          <div className={classNames('select')}>
+            {
+              // This is done in order for default values to be selected correctly
+              // if the options are not present at init, it will never be selected
+              loading ? (
+                <select key="select_service_disabled" disabled />
+              ) : (
+                <select
+                  key="select_service"
+                  id="services"
+                  defaultValue={selectedService}
+                  {...serviceProps}
+                >
+                  <option value="">
+                    {t('common:subscribe_form.fields.service.default', 'Select...')}
                   </option>
-                ))}
-              </select>
-            )
-          }
-          <FiChevronDown color="#333333"></FiChevronDown>
+                  <optgroup>
+                    {dmaServices.map((service) => (
+                      <option
+                        key={`${selectedService}_${service}`}
+                        value={service}
+                        selected={service === selectedService}
+                      >
+                        {service}
+                      </option>
+                    ))}
+                  </optgroup>
+                  <optgroup>
+                    {otherServices.map((service) => (
+                      <option
+                        key={`${selectedService}_${service}`}
+                        value={service}
+                        selected={service === selectedService}
+                      >
+                        {service}
+                      </option>
+                    ))}
+                  </optgroup>
+                </select>
+              )
+            }
+            <FiChevronDown color="#333333"></FiChevronDown>
+          </div>
         </div>
-      </div>
-      <div className={classNames('formfield')}>
-        <label htmlFor="documentTypes">
-          {t('common:subscribe_form.fields.document.label', 'Select a document type')}
-        </label>
-        <div className={classNames('select')}>
-          {
-            // This is done in order for default values to be selected correctly
-            // if the options are not present at init, it will never be selected
-            loading ? (
-              <select key="select_documentTypes_disabled" disabled />
-            ) : (
-              <select
-                key={`select_documentTypes`}
-                id="documentTypes"
-                defaultValue={selectedDocumentType}
-                {...documentTypeProps}
-              >
-                <option value="">
-                  {t('common:subscribe_form.fields.document.default', 'Select...')}
-                </option>
-                {documentTypes.map((documentType) => (
-                  <option key={`${selectedService}_${documentType}`} value={documentType}>
-                    {documentType}
+        <div className={classNames('formfield')}>
+          <label htmlFor="documentTypes">
+            {t('common:subscribe_form.fields.document.label', 'Select a document type')}
+          </label>
+          <div className={classNames('select')}>
+            {
+              // This is done in order for default values to be selected correctly
+              // if the options are not present at init, it will never be selected
+              loading ? (
+                <select key="select_documentTypes_disabled" disabled />
+              ) : (
+                <select
+                  key={`select_documentTypes`}
+                  id="documentTypes"
+                  defaultValue={selectedDocumentType}
+                  {...documentTypeProps}
+                >
+                  <option value="">
+                    {t('common:subscribe_form.fields.document.default', 'Select...')}
                   </option>
-                ))}
-              </select>
-            )
-          }
-          <FiChevronDown color="#333333"></FiChevronDown>
+                  {documentTypes.map((documentType) => (
+                    <option key={`${selectedService}_${documentType}`} value={documentType}>
+                      {documentType}
+                    </option>
+                  ))}
+                </select>
+              )
+            }
+            <FiChevronDown color="#333333"></FiChevronDown>
+          </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+);
 
 export default SelectService;
