82f712cbd2a4f9d0c36a2799f255b48bdb9dc046
Martin Ratinaud <martinratinaud@gmail.com>
2021-08-06 08:05:46 +0400

    chore(api): add endpoint to create a github issue on contribution submission

---
+++ src/modules/Contribute/managers/ServiceManager.ts
@@ -0,0 +1,68 @@
+import { addCommentToIssue, createIssue, searchIssue } from 'modules/Github/api';
+
+const GITHUB_OTA_OWNER = process.env.GITHUB_OTA_OWNER || '';
+const GITHUB_OTA_REPO = process.env.GITHUB_OTA_REPO || '';
+
+const commonParams = {
+  owner: GITHUB_OTA_OWNER,
+  repo: GITHUB_OTA_REPO,
+  accept: 'application/vnd.github.v3+json',
+};
+
+export const addService = async ({
+  name,
+  documentType,
+  json,
+  url,
+}: {
+  name: string;
+  documentType: string;
+  json: any;
+  url: string;
+}) => {
+  const issueTitle = `Add ${name} - ${documentType}`;
+  const issueBodyCommon = `
+You can see the work done by the awesome contributor here:
+${url}
+
+Or you can see the JSON generated here:
+\`\`\`json
+${JSON.stringify(json, null, 2)}
+\`\`\`
+
+You will need to create the following file in the root of the project: \`services/${name}.json\`
+
+`;
+
+  let existingIssue = await searchIssue({
+    ...commonParams,
+    // baseUrl should be the way to go but it goes with a 404 using octokit
+    // baseUrl: `https://api.github.com/${GITHUB_OTA_OWNER}/${GITHUB_OTA_REPO}`,
+    q: `is:issue "${issueTitle}"`,
+  });
+
+  if (existingIssue) {
+    await addCommentToIssue({
+      ...commonParams,
+      issue_number: existingIssue.number,
+      body: `
+Addition has been requested again through the contribution tool
+
+${issueBodyCommon}
+`,
+    });
+  } else {
+    existingIssue = await createIssue({
+      ...commonParams,
+      title: issueTitle,
+      body: `
+New service addition requested through the contribution tool
+
+${issueBodyCommon}
+`,
+      labels: ['add-document', 'add-service'],
+    });
+  }
+
+  return existingIssue;
+};
--- src/modules/Github/api/index.ts
+++ src/modules/Github/api/index.ts
@@ -1,7 +1,10 @@
 const DOCUMENT_TYPES_URL =
   'https://raw.githubusercontent.com/ambanum/OpenTermsArchive/master/src/app/types.json';
 
+import { Octokit } from 'octokit';
 import axios from 'axios';
+
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN_CREATE_ISSUE });
 
 export const getDocumentTypes: any = async () => {
   try {
@@ -10,5 +13,51 @@ export const getDocumentTypes: any = async () => {
   } catch (e) {
     console.error(e);
     return [];
+  }
+};
+
+export const createIssue: any = async (
+  params: Parameters<typeof octokit.rest.issues.create>[0]
+) => {
+  try {
+    const { data } = await octokit.rest.issues.create(params);
+
+    return data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const searchIssue = async (
+  params: Parameters<typeof octokit.rest.search.issuesAndPullRequests>[0] & {
+    owner: string;
+    repo: string;
+  }
+) => {
+  try {
+    const { data } = await octokit.rest.search.issuesAndPullRequests(params);
+
+    // baseUrl should be the way to go instead of this ugly filter
+    // that may not work in case there are too many issues
+    // but it goes with a 404 using octokit
+    // baseUrl: `https://api.github.com/${GITHUB_OTA_OWNER}/${GITHUB_OTA_REPO}`,
+    return (data?.items || []).filter((item) =>
+      item.repository_url.endsWith(`${params.owner}/${params.repo}`)
+    )[0];
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+export const addCommentToIssue = async (
+  params: Parameters<typeof octokit.rest.issues.createComment>[0]
+) => {
+  try {
+    const { data } = await octokit.rest.issues.createComment(params);
+    return data;
+  } catch (e) {
+    console.error(e);
+    return null;
   }
 };
