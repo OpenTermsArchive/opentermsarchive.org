a5ad17b2343c77756e73a6255f0e281a219537e8
Martin Ratinaud <martinratinaud@gmail.com>
2021-09-23 14:14:12 +0400

    feat: add more contributors

--- src/modules/OTA-api/data-components/Contributors.tsx
+++ src/modules/OTA-api/data-components/Contributors.tsx
@@ -3,11 +3,38 @@ import Link from 'next/link';
 import Loading from 'components/Loading';
 import React from 'react';
 import s from './Contributors.module.css';
+import shuffle from 'lodash/fp/shuffle';
 import useSWR from 'swr';
 
 type ContributorsProps = {
   subtitle: string;
 } & React.HTMLAttributes<HTMLDivElement>;
+
+const outsideContributors = [
+  {
+    html_url: 'https://github.com/trujilloelsa',
+    avatar_url: 'https://avatars.githubusercontent.com/u/86837188?v=4',
+    login: 'Elsa Trujillo',
+  },
+  {
+    html_url: 'https://twitter.com/marineguillaum?lang=en',
+    avatar_url:
+      'https://pbs.twimg.com/profile_images/815851313758085121/oNANphLi_reasonably_small.jpg',
+    login: 'Marine Guillaume',
+  },
+  {
+    html_url: 'https://twitter.com/lvdefranssu',
+    avatar_url:
+      'https://pbs.twimg.com/profile_images/1435510938979942405/GZUnqvtH_reasonably_small.jpg',
+    login: 'Louis-Victor de Franssu',
+  },
+  {
+    html_url: 'https://twitter.com/henriverdier',
+    avatar_url:
+      'https://pbs.twimg.com/profile_images/1359173368436686848/1GNqcMOf_reasonably_small.jpg',
+    login: 'Henri Verdier',
+  },
+];
 
 const Contributors: React.FC<ContributorsProps> = React.memo(({ subtitle }) => {
   const { data } = useSWR('/api/ota/contributors', {
@@ -20,17 +47,13 @@ const Contributors: React.FC<ContributorsProps> = React.memo(({ subtitle }) => {
     return <Loading />;
   }
 
-  data.unshift({
-    html_url: 'https://github.com/trujilloelsa',
-    avatar_url: 'https://avatars.githubusercontent.com/u/86837188?v=4',
-    login: 'Elsa Trujillo',
-  });
+  const contributors = shuffle([...outsideContributors, ...data]);
 
   return (
     <div className={s.contributors}>
       <div className="text__smallcaps text__center">{subtitle}</div>
       <div className={s.contributors_items}>
-        {data.map((contributor: Contributor) => {
+        {contributors.map((contributor: Contributor) => {
           if (contributor.type !== 'Bot') {
             return (
               <div className={s.contributor}>
