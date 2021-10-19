131b1f8bf9fa57b1fa764b1f811664a0354e3942
Clément Biron <clement.biron@gmail.com>
2021-10-19 11:13:32 +0200

    feat: add latest change record on dashboard

--- src/modules/Github/api/index.ts
+++ src/modules/Github/api/index.ts
@@ -1,12 +1,78 @@
 const DOCUMENT_TYPES_URL =
   'https://opentermsarchive.org/data/api/list_documentTypes/v1/';
 export const CONTRIBUTORS_URL = 'https://api.github.com/repos/ambanum/OpenTermsArchive/contributors';
-export const VERSIONS_CONTRIBUTOR_COMMIT_ACTIVITY = 'https://api.github.com/repos/ambanum/OpenTermsArchive-versions/stats/contributors';
+export const VERSIONS_CONTRIBUTOR_COMMITS_ACTIVITY = 'https://api.github.com/repos/ambanum/OpenTermsArchive-versions/stats/contributors';
+export const LAST_VERSIONS_COMMITS = 'https://api.github.com/repos/ambanum/OpenTermsArchive-versions/commits';
 
 import { Octokit } from 'octokit';
 import axios from 'axios';
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN_CREATE_ISSUE });
+
+export interface GitHubAuthor {
+  login: string;
+  id: number;
+  node_id: number;
+  avatar_url: string;
+  gravatar_id: string;
+  url:string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  susbscriptions_url: string;
+  organizations_url: string;
+  repos_url:string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+export interface ContributorActivityWeek{
+  w:number; //Start of the week, given as a Unix timestamp.
+  a:number; //Number of additions
+  d:number; //Number of deletions
+  c:number; //Number of commits
+}
+
+export interface ContributorActivity {
+  total:number;
+  weeks:ContributorActivityWeek[];
+  author:GitHubAuthor;
+}
+
+export interface Commit{
+  url: string;
+  sha: string;
+  html_url: string;
+  commit:{
+    message:string;
+    author:{
+      date:string;
+    }
+  }
+  author:{
+    login: string;
+    avatar_url: string;
+    html_url: string;
+  }
+}
+
+export interface Commits{
+  commits:Commit[]
+}
+
+export interface Contributor {
+  login: string;
+  avatar_url: string;
+  html_url: string;
+  contributions: number;
+  type: string;
+}
+
+export type Contributors = Contributor[];
 
 export const getDocumentTypes: any = async () => {
   try {
@@ -63,16 +129,6 @@ export const addCommentToIssue = async (
   }
 };
 
-export interface Contributor {
-  login: string;
-  avatar_url: string;
-  html_url: string;
-  contributions: number;
-  type: string;
-}
-
-export type Contributors = Contributor[];
-
 export const getContributors = async () => {
   try {
     const  { data } : { data : Contributors }  = await octokit.request(`GET ${CONTRIBUTORS_URL}`)
@@ -83,43 +139,20 @@ export const getContributors = async () => {
   }
 }
 
-export interface GitHubAuthor {
-  login: string;
-  id: number;
-  node_id: number;
-  avatar_url: string;
-  gravatar_id: string;
-  url:string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  susbscriptions_url: string;
-  organizations_url: string;
-  repos_url:string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
-}
-
-export interface ContributorActivityWeek{
-  w:number; //Start of the week, given as a Unix timestamp.
-  a:number; //Number of additions
-  d:number; //Number of deletions
-  c:number; //Number of commits
-}
-
-export interface ContributorActivity {
-  total:number;
-  weeks:ContributorActivityWeek[];
-  author:GitHubAuthor;
-}
-
 export const getAllVersionsContributorCommitActivity = async () => {
   try {
-    const  { data } : { data: ContributorActivity } = await octokit.request(`GET ${VERSIONS_CONTRIBUTOR_COMMIT_ACTIVITY}`);
+    const  { data } : { data: ContributorActivity } = await octokit.request(`GET ${VERSIONS_CONTRIBUTOR_COMMITS_ACTIVITY}`);
+    return data;
+  } catch (e) {
+    console.error(e);
+    return {};
+  }
+}
+
+export const getLastVersionsCommits = async () => {
+  try {
+    const per_page = 10;
+    const  { data } : { data: Commits } = await octokit.request(`GET ${LAST_VERSIONS_COMMITS}`, { per_page });
     return data;
   } catch (e) {
     console.error(e);
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,9 @@
 import {
+  Commit,
   ContributorActivity,
   ContributorActivityWeek,
   getAllVersionsContributorCommitActivity,
+  getLastVersionsCommits,
 } from 'modules/Github/api';
 
 import Column from 'modules/Common/components/Column';
@@ -9,184 +11,227 @@ import Container from 'modules/Common/containers/Container';
 import Hero from 'modules/Common/components/Hero';
 import Layout from 'modules/Common/containers/Layout';
 import LineChart from 'modules/Common/components/Charts/LineChart';
+import LinkIcon from 'modules/Common/components/LinkIcon';
 import React from 'react';
 import TextContent from 'modules/Common/components/TextContent';
 import { getGraphServices } from 'modules/OTA-api/api';
 import { useTranslation } from 'next-i18next';
 import { withI18n } from 'modules/I18n';
 
-const DashboardPage = React.memo(({ graphServices, versionsContributorCommitActivity }: any) => {
-  const { t } = useTranslation('common');
+const DashboardPage = React.memo(
+  ({
+    graphServices,
+    versionsContributorCommitActivity,
+    lastVersionsCommits: lastestVersionsCommits,
+  }: any) => {
+    const { t } = useTranslation('common');
 
-  const cuttOffDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
-  const firstDayOfCurrentMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+    const cuttOffDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+    const firstDayOfCurrentMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
 
-  //Total number of versions commits
-  // const totalVersionsCommits = versionsContributorCommitActivity
-  //   .map((contributorActivity: ContributorActivity) => {
-  //     return contributorActivity.total;
-  //   })
-  //   .reduce((pv: number, cv: number) => {
-  //     return pv + cv;
-  //   });
+    //Total number of versions commits
+    // const totalVersionsCommits = versionsContributorCommitActivity
+    //   .map((contributorActivity: ContributorActivity) => {
+    //     return contributorActivity.total;
+    //   })
+    //   .reduce((pv: number, cv: number) => {
+    //     return pv + cv;
+    //   });
 
-  //Format commits versions data
-  const tempVersionsCommitsData = versionsContributorCommitActivity
-    .map((contributorActivity: ContributorActivity) => {
-      return contributorActivity.weeks;
-    })
-    .flat()
-    .sort((a: ContributorActivityWeek, b: ContributorActivityWeek) => (a.w > b.w ? 1 : -1))
-    .reduce((acc: any, week: ContributorActivityWeek) => {
-      if (week.c > 0) {
-        const currentDate = new Date(week.w * 1000);
-        if (
-          currentDate.getTime() >= cuttOffDate.getTime() &&
-          currentDate.getTime() < firstDayOfCurrentMonth.getTime()
-        ) {
-          const fullYear = currentDate.getFullYear();
-          const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
-          const formatedDate = `${fullYear}-${month}`;
-          const chartData = {
-            x: formatedDate,
-            y: week.c,
-          };
+    //Format commits versions data
+    const tempVersionsCommitsData = versionsContributorCommitActivity
+      .map((contributorActivity: ContributorActivity) => {
+        return contributorActivity.weeks;
+      })
+      .flat()
+      .sort((a: ContributorActivityWeek, b: ContributorActivityWeek) => (a.w > b.w ? 1 : -1))
+      .reduce((acc: any, week: ContributorActivityWeek) => {
+        if (week.c > 0) {
+          const currentDate = new Date(week.w * 1000);
+          if (
+            currentDate.getTime() >= cuttOffDate.getTime() &&
+            currentDate.getTime() < firstDayOfCurrentMonth.getTime()
+          ) {
+            const fullYear = currentDate.getFullYear();
+            const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+            const formatedDate = `${fullYear}-${month}`;
+            const chartData = {
+              x: formatedDate,
+              y: week.c,
+            };
 
-          if (acc[formatedDate] === undefined) {
-            acc[formatedDate] = chartData;
-          } else if (acc[formatedDate].x === formatedDate) {
-            acc[formatedDate].y += week.c;
+            if (acc[formatedDate] === undefined) {
+              acc[formatedDate] = chartData;
+            } else if (acc[formatedDate].x === formatedDate) {
+              acc[formatedDate].y += week.c;
+            }
           }
         }
-      }
-      return acc;
-    }, {});
-  const versionsCommitsData = Object.keys(tempVersionsCommitsData).map(
-    (key) => tempVersionsCommitsData[key]
-  );
+        return acc;
+      }, {});
+    const versionsCommitsData = Object.keys(tempVersionsCommitsData).map(
+      (key) => tempVersionsCommitsData[key]
+    );
 
-  //Format data for line graph services
-  const trackedServicesData: any[] = [];
-  const activeServicesData: any[] = [];
-  if (graphServices) {
-    graphServices.forEach((graphService: any) => {
-      const graphServiceDate = new Date(
-        new Date(graphService.year_month).getFullYear(),
-        new Date(graphService.year_month).getMonth() + 1,
-        0
-      );
+    //Format data for line graph services
+    const trackedServicesData: any[] = [];
+    const activeServicesData: any[] = [];
+    if (graphServices) {
+      graphServices.forEach((graphService: any) => {
+        const graphServiceDate = new Date(
+          new Date(graphService.year_month).getFullYear(),
+          new Date(graphService.year_month).getMonth() + 1,
+          0
+        );
 
-      if (
-        graphServiceDate.getTime() >= cuttOffDate.getTime() &&
-        graphServiceDate.getTime() < firstDayOfCurrentMonth.getTime()
-      ) {
-        trackedServicesData.push({
-          x: graphService.year_month,
-          y: graphService.n_services_tracked,
-        });
-        activeServicesData.push({
-          x: graphService.year_month,
-          y: graphService.n_services_active,
-        });
-      }
-    });
-  }
+        if (
+          graphServiceDate.getTime() >= cuttOffDate.getTime() &&
+          graphServiceDate.getTime() < firstDayOfCurrentMonth.getTime()
+        ) {
+          trackedServicesData.push({
+            x: graphService.year_month,
+            y: graphService.n_services_tracked,
+          });
+          activeServicesData.push({
+            x: graphService.year_month,
+            y: graphService.n_services_active,
+          });
+        }
+      });
+    }
 
-  const documentsVersionsLegend: string = t(
-    'common:dashboard.activity.chart.legend.documentsversions',
-    'Documents versions'
-  );
+    const documentsVersionsLegend: string = t(
+      'common:dashboard.activity.chart.legend.documentsversions',
+      'Documents versions'
+    );
 
-  const trackedServicesLegend: string = t(
-    'common:dashboard.activity.chart.legend.trackedservices',
-    'Tracked services'
-  );
-  const activeServicesLegend: string = t(
-    'common:dashboard.activity.chart.legend.activeservices',
-    'Active services'
-  );
-  const servicesAxisBottomLegend: string = t(
-    'common:dashboard.activity.chart.axis.bottom.legend',
-    'year-month'
-  );
-  const servicesAxisLeftLegend: string = t(
-    'common:dashboard.activity.chart.axis.left.legend',
-    'number'
-  );
+    const trackedServicesLegend: string = t(
+      'common:dashboard.activity.chart.legend.trackedservices',
+      'Tracked services'
+    );
+    const activeServicesLegend: string = t(
+      'common:dashboard.activity.chart.legend.activeservices',
+      'Active services'
+    );
+    const servicesAxisBottomLegend: string = t(
+      'common:dashboard.activity.chart.axis.bottom.legend',
+      'year-month'
+    );
+    const servicesAxisLeftLegend: string = t(
+      'common:dashboard.activity.chart.axis.left.legend',
+      'number'
+    );
 
-  return (
-    <Layout title={t('common:dashboard.seo.title', 'Dashboard')}>
-      <Container layout="wide" paddingY={false} gray={true}>
-        <Container gridCols="12" gridGutters="11" flex={true} paddingX={false}>
-          <Hero
-            title={t('common:dashboard.hero.title', 'Dashboard')}
-            subtitle={t(
-              'common:dashboard.hero.subtitle',
-              'A set of activity metrics for Open Terms Archive'
-            )}
-          ></Hero>
-        </Container>
-      </Container>
-
-      <Container gridCols="12" gridGutters="11">
-        <Column
-          width={100}
-          title={t('common:dashboard.activity.title', 'Activity')}
-          subtitle={t('common:dashboard.activity.subtitle', `Last 12 months activity`)}
-        >
-          <TextContent>
-            <p>
-              {t(
-                'common:dashboard.activity.desc',
-                `Evolution of services and documents versions over the last 12 months`
+    return (
+      <Layout title={t('common:dashboard.seo.title', 'Dashboard')}>
+        <Container layout="wide" paddingY={false} gray={true}>
+          <Container gridCols="12" gridGutters="11" flex={true} paddingX={false}>
+            <Hero
+              title={t('common:dashboard.hero.title', 'Dashboard')}
+              subtitle={t(
+                'common:dashboard.hero.subtitle',
+                'A set of activity metrics for Open Terms Archive'
               )}
-            </p>
-          </TextContent>
-          <LineChart
-            height={580}
-            data={[
-              {
-                id: documentsVersionsLegend,
-                data: versionsCommitsData,
-              },
-              {
-                id: trackedServicesLegend,
-                data: trackedServicesData,
-              },
-              {
-                id: activeServicesLegend,
-                data: activeServicesData,
-              },
-            ]}
-            axisBottom={{
-              tickSize: 8,
-              tickPadding: 8,
-              tickRotation: 0,
-              legend: servicesAxisBottomLegend,
-              legendOffset: 44,
-              legendPosition: 'middle',
-            }}
-            axisLeft={{
-              tickSize: 8,
-              tickPadding: 8,
-              tickRotation: 0,
-              legend: servicesAxisLeftLegend,
-              legendOffset: -48,
-              legendPosition: 'middle',
-            }}
-          />
-        </Column>
-      </Container>
-    </Layout>
-  );
-});
+            ></Hero>
+          </Container>
+        </Container>
+
+        <Container gridCols="12" gridGutters="11">
+          <Column
+            width={100}
+            title={t('common:dashboard.activity.title', 'Activity')}
+            subtitle={t('common:dashboard.activity.subtitle', `Last 12 months activity`)}
+          >
+            <TextContent>
+              <p>
+                {t(
+                  'common:dashboard.activity.desc',
+                  `Evolution of services and documents versions over the last 12 months`
+                )}
+              </p>
+            </TextContent>
+            {versionsCommitsData && trackedServicesData && activeServicesData && (
+              <LineChart
+                height={580}
+                data={[
+                  {
+                    id: documentsVersionsLegend,
+                    data: versionsCommitsData,
+                  },
+                  {
+                    id: trackedServicesLegend,
+                    data: trackedServicesData,
+                  },
+                  {
+                    id: activeServicesLegend,
+                    data: activeServicesData,
+                  },
+                ]}
+                axisBottom={{
+                  tickSize: 8,
+                  tickPadding: 8,
+                  tickRotation: 0,
+                  legend: servicesAxisBottomLegend,
+                  legendOffset: 44,
+                  legendPosition: 'middle',
+                }}
+                axisLeft={{
+                  tickSize: 8,
+                  tickPadding: 8,
+                  tickRotation: 0,
+                  legend: servicesAxisLeftLegend,
+                  legendOffset: -48,
+                  legendPosition: 'middle',
+                }}
+              />
+            )}
+          </Column>
+        </Container>
+
+        <Container gridCols="12" gridGutters="11">
+          <Column
+            width={100}
+            title={t('common:dashboard.lastestversionscommits.title', 'Latest changes recorded')}
+            subtitle={t(
+              'common:dashboard.lastestversionscommits.subtitle',
+              'on the tracked documents'
+            )}
+          >
+            <TextContent>
+              <ul>
+                {lastestVersionsCommits.map((versionCommit: Commit) => {
+                  const splittedMessage = versionCommit.commit.message.split('\n\n');
+                  const date = new Date(versionCommit.commit.author.date);
+                  return (
+                    <li>
+                      <LinkIcon target="_blank" href={versionCommit.html_url}>
+                        {splittedMessage[0]}
+                      </LinkIcon>{' '}
+                      <span className="text__light">({date.toUTCString()})</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </TextContent>
+          </Column>
+        </Container>
+      </Layout>
+    );
+  }
+);
 
 export const getStaticProps = withI18n()(async (props: any) => {
   const graphServices = await getGraphServices();
   const versionsContributorCommitActivity = await getAllVersionsContributorCommitActivity();
+  const lastestVersionsCommits = await getLastVersionsCommits();
   return JSON.parse(
     JSON.stringify({
-      props: { ...props, graphServices, versionsContributorCommitActivity },
+      props: {
+        ...props,
+        graphServices,
+        versionsContributorCommitActivity,
+        lastVersionsCommits: lastestVersionsCommits,
+      },
       revalidate: 60 * 60 * 1,
     })
   );
