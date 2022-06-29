ca38d2bf85f2d926ea14413b7a723475466c992a
Martin Ratinaud <martinratinaud@gmail.com>
2022-06-29 10:29:33 +0400

    Fix Centent Security Policy error

--- next.config.js
+++ next.config.js
@@ -66,4 +66,23 @@ module.exports = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              // object-src 'none'. Recommended on https://csp-evaluator.withgoogle.com/
+              // style-src 'unsafe-inline'. because nextjs inlines CSS
+              // img-src data:. because next/image component inlines data images
+              // img-src http*. for all images in contributors list
+              // script-src 'unsafe-eval'. because next or one package needs it
+              "default-src 'self'; script-src 'self' 'unsafe-eval' https://stats.data.gouv.fr; object-src 'none'; style-src 'self' 'unsafe-inline'; img-src 'self'  https://media-exp1.licdn.com data: https://www.gravatar.com https://avatars.githubusercontent.com https://pbs.twimg.com https://sibyll.in; frame-src 'self' https://stats.data.gouv.fr",
+          },
+        ],
+      },
+    ];
+  },
 };
