23bf54bcabaf5951b856dca52ec5cc6103ae312c
Martin Ratinaud <martinratinaud@gmail.com>
2022-07-04 08:47:41 +0400

    Refactor locale handling condition
    For easier reading

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -7,14 +7,18 @@ const PUBLIC_FILE = /\.([a-zA-Z]+$)/;
 export function middleware(request: NextRequest) {
   const { pathname, search, locale } = request.nextUrl;
 
-  if (pathname.includes('/api/') || pathname.includes('/fonts/')) {
+  const shouldHandleLocale =
+    !PUBLIC_FILE.test(pathname) &&
+    locale === 'default' &&
+    !pathname.includes('/api/') &&
+    !pathname.includes('/fonts/');
+
+  if (!shouldHandleLocale) {
     return;
   }
-
-  const shouldHandleLocale = !PUBLIC_FILE.test(pathname) && locale === 'default';
 
   acceptLanguage.languages(i18n.locales?.filter((locale) => locale !== 'default'));
   const detectedLocale = acceptLanguage.get(request.headers.get('accept-language'));
 
-  return shouldHandleLocale && NextResponse.redirect(`/${detectedLocale}${pathname}${search}`);
+  return NextResponse.redirect(`/${detectedLocale}${pathname}${search}`);
 }
