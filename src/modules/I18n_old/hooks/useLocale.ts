89b9a423741163d573d279ba23056647da4a6e29
Martin Ratinaud <martinratinaud@gmail.com>
2022-10-20 15:19:31 +0400

    Remove next-i18next

--- src/modules/I18n_old/hooks/useLocale.ts
+++
@@ -1,9 +0,0 @@
-import 'dayjs/locale/fr';
-import dayjs from 'dayjs';
-import localeData from 'dayjs/plugin/localeData';
-dayjs.extend(localeData);
-dayjs().localeData();
-
-export default function useLocale(locale: string) {
-  dayjs.locale(locale);
-}
