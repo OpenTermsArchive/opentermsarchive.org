8959f9876dcfd536582c88917b41c60c0f2cbabe
Martin Ratinaud <martinratinaud@gmail.com>
2022-10-26 10:31:55 +0400

    Add Hero support on static pages

--- src/pages/[...paths].tsx
+++ src/pages/[...paths].tsx
@@ -2,6 +2,8 @@ import { WithMdxResult, loadMdxFile, getStaticFilesPaths } from 'modules/I18n/ho
 import Button from 'modules/Common/components/Button';
 import Container from 'modules/Common/containers/Container';
 import Layout from 'modules/Common/containers/Layout';
+import LinkIcon from 'modules/Common/components/LinkIcon';
+import Hero from 'modules/Common/components/Hero';
 import { MDXRemote } from 'next-mdx-remote';
 import React from 'react';
 import type { GetStaticPaths, GetStaticProps, GetStaticPathsResult } from 'next';
@@ -10,13 +12,21 @@ import TextContent from 'modules/Common/components/TextContent';
 const FOLDER = 'static';
 
 export default function PrivacyPolicyPage({ mdxContent }: WithMdxResult) {
-  const { frontmatter } = mdxContent || {};
+  const { frontmatter = {} } = mdxContent || {};
 
   return (
     <Layout title={frontmatter?.title} desc={frontmatter?.description}>
+      {frontmatter['hero.title'] && (
+        <Container layout="wide" dark={true} paddingY={false}>
+          <Container gridCols="12" gridGutters="11" flex={true} paddingX={false}>
+            <Hero title={frontmatter['hero.title']} subtitle={frontmatter['hero.subtitle']}></Hero>
+          </Container>
+        </Container>
+      )}
+
       <Container gridCols="10" gridGutters="9">
         <TextContent>
-          {mdxContent && <MDXRemote {...mdxContent} components={{ Button }} />}
+          {mdxContent && <MDXRemote {...mdxContent} components={{ Button, LinkIcon }} />}
         </TextContent>
       </Container>
     </Layout>
@@ -37,7 +47,7 @@ export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
       ],
       [] as GetStaticPathsResult['paths']
     ),
-    fallback: false,
+    fallback: 'blocking',
   };
 };
 
