fbd0cb4a7986009b9c756029da95a79b17a8fe9e
Martin Ratinaud <martinratinaud@gmail.com>
2022-11-16 18:12:06 +0400

    Export all function from modules/I18n

---
+++ src/modules/I18n/index.tsx
@@ -0,0 +1,4 @@
+export { default as Link } from './components/Link';
+export { default as getLinkAlternates } from './utils/getLinkAlternates';
+export { default as useTranslation } from './hooks/useTranslation';
+export { default as useLocale } from './hooks/useLocale';
