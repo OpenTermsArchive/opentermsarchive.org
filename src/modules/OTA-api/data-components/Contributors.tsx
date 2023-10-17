523fdaf846458e8ae3927d86447d6c988d1f99af
Clément Biron <clement.biron@gmail.com>
2023-10-17 10:08:01 +0200

    Map all-contributorsrc data to display them

--- src/modules/OTA-api/data-components/Contributors.tsx
+++ src/modules/OTA-api/data-components/Contributors.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import staffData from '../../../../public/staff.json';
+import allContributorsData from '../../../../.all-contributorsrc';
 import s from './Contributors.module.css';
 import slugify from 'slugify';
 
@@ -14,16 +15,40 @@ type ContributorsProps = {
   marginTop?: boolean;
 } & React.HTMLAttributes<HTMLDivElement>;
 
+const mapAllContributorsData = (
+  allContributors: {
+    name: string;
+    profile: string;
+    avatar_url: string;
+  }[]
+) => {
+  const contributorsShemed: {
+    [name: string]: {
+      website: string;
+      picture?: string;
+    };
+  } = {};
+  Object.values(allContributors).map((contributor: any) => {
+    contributorsShemed[contributor.name] = {
+      website: contributor.profile,
+    };
+    if (contributor.avatar_url != '') {
+      contributorsShemed[contributor.name].picture = contributor.avatar_url;
+    }
+  });
+  return contributorsShemed;
+};
+
 const getContributorsByType = (type: ContributorsProps['type']) => {
   switch (type) {
     case 'core':
       return staffData.core;
     case 'alumnis':
       return staffData.alumnis;
-    case 'contributors':
-      return staffData.contributors;
     case 'communityManagers':
       return staffData.communityManagers;
+    case 'contributors':
+      return mapAllContributorsData(allContributorsData.contributors);
     case 'all':
     default:
       return staffData;
@@ -43,7 +68,6 @@ const Contributors: React.FC<ContributorsProps> = React.memo(
     ...props
   }) => {
     const contributors = getContributorsByType(type);
-
     return (
       <div
         className={classNames(
@@ -59,20 +83,25 @@ const Contributors: React.FC<ContributorsProps> = React.memo(
       >
         {subtitle && <h4 className={classNames(s.contributors_subtitle)}>{subtitle}</h4>}
         <div className={s.contributors_items}>
-          {Object.entries(contributors).map(([name, url]) => {
+          {Object.entries(contributors).map(([name, value]) => {
+            const website = typeof value === 'object' ? value.website : value;
+            const imgSrc =
+              typeof value === 'object' && value.picture != null
+                ? value.picture
+                : `/images/contributors/${slugify(name, { lower: true })}.jpg`;
             return (
               <div className={s.contributor} key={`${name}`}>
                 <a
                   target="_blank"
                   rel="nofollow noopener"
                   className={s.contributor_link}
-                  title={url}
-                  key={`${url}_link`}
-                  href={url}
+                  title={website}
+                  key={`${name}_link`}
+                  href={website}
                 >
                   <img
                     className={s.contributor_image}
-                    src={`/images/contributors/${slugify(name, { lower: true })}.jpg`}
+                    src={imgSrc}
                     alt={name}
                     width={thumbnailWidth}
                     height={thumbnailHeight}
