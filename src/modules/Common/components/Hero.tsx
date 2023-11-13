b30fb6d031405ba019fa84b62d53780995f6e046
Clément Biron <clement.biron@gmail.com>
2023-11-13 11:29:02 +0100

    Allow no margin bottom

--- src/modules/Common/components/Hero.tsx
+++ src/modules/Common/components/Hero.tsx
@@ -6,13 +6,32 @@ type HeroProps = {
   className?: string;
   title?: string;
   subtitle?: string;
+  marginBottom?: boolean;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-const Hero: React.FC<HeroProps> = ({ children, className, title, subtitle, ...props }) => {
+const Hero: React.FC<HeroProps> = ({
+  children,
+  className,
+  title,
+  subtitle,
+  marginBottom = true,
+  ...props
+}) => {
   return (
-    <div className={classNames(s.hero, className)} {...props}>
-      {title &&  <h1 className={s.hero_title} dangerouslySetInnerHTML={{ __html: title }}></h1>}
-      {subtitle && <h2 className={classNames(s.hero_subtitle, 'h2__thin')}>{subtitle} {children}</h2>}
+    <div
+      className={classNames(
+        s.hero,
+        { [s.hero__hasNoMarginBottom]: marginBottom === false },
+        className
+      )}
+      {...props}
+    >
+      {title && <h1 className={s.hero_title} dangerouslySetInnerHTML={{ __html: title }}></h1>}
+      {subtitle && (
+        <h2 className={classNames(s.hero_subtitle, 'h2__thin')}>
+          {subtitle} {children}
+        </h2>
+      )}
     </div>
   );
 };
