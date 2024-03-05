09edd58224af572aeb37adbcc5511ae5ffd4f224
Clément Biron <clement.biron@gmail.com>
2024-03-05 22:19:28 +0100

    Setup Lucide icons

---
+++ themes/opentermsarchive/assets/js/lucide.js
@@ -0,0 +1,38 @@
+import {
+  Github,
+  Twitter,
+  ChevronDown,
+  X,
+  Hash,
+  Mail,
+  Folder,
+  File,
+  MessageCircle,
+  Search,
+  Download,
+  Wrench,
+  Users,
+  User,
+  Map,
+  createIcons,
+} from 'lucide';
+
+createIcons({
+  icons: {
+    Twitter,
+    X,
+    ChevronDown,
+    Github,
+    Hash,
+    Mail,
+    Folder,
+    File,
+    MessageCircle,
+    Search,
+    Download,
+    Wrench,
+    Users,
+    User,
+    Map
+  }
+});
