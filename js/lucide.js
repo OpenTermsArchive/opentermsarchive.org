ed1a6906b255fe0d17d80f5ba65d24e289b9e632
clementbiron <clementbiron@users.noreply.github.com>
2024-03-15 13:41:41 +0000

    deploy: 5133d1d6be8d8aa1237c493955e42ca838d3a1b1

---
+++ js/lucide.js
@@ -0,0 +1,543 @@
+(() => {
+  // node_modules/lucide/dist/esm/createElement.js
+  var createElement = (tag, attrs, children = []) => {
+    const element = document.createElementNS("http://www.w3.org/2000/svg", tag);
+    Object.keys(attrs).forEach((name) => {
+      element.setAttribute(name, String(attrs[name]));
+    });
+    if (children.length) {
+      children.forEach((child) => {
+        const childElement = createElement(...child);
+        element.appendChild(childElement);
+      });
+    }
+    return element;
+  };
+  var createElement$1 = ([tag, attrs, children]) => createElement(tag, attrs, children);
+
+  // node_modules/lucide/dist/esm/replaceElement.js
+  var getAttrs = (element) => Array.from(element.attributes).reduce((attrs, attr) => {
+    attrs[attr.name] = attr.value;
+    return attrs;
+  }, {});
+  var getClassNames = (attrs) => {
+    if (typeof attrs === "string")
+      return attrs;
+    if (!attrs || !attrs.class)
+      return "";
+    if (attrs.class && typeof attrs.class === "string") {
+      return attrs.class.split(" ");
+    }
+    if (attrs.class && Array.isArray(attrs.class)) {
+      return attrs.class;
+    }
+    return "";
+  };
+  var combineClassNames = (arrayOfClassnames) => {
+    const classNameArray = arrayOfClassnames.flatMap(getClassNames);
+    return classNameArray.map((classItem) => classItem.trim()).filter(Boolean).filter((value, index, self) => self.indexOf(value) === index).join(" ");
+  };
+  var toPascalCase = (string) => string.replace(/(\w)(\w*)(_|-|\s*)/g, (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase());
+  var replaceElement = (element, { nameAttr, icons, attrs }) => {
+    const iconName = element.getAttribute(nameAttr);
+    if (iconName == null)
+      return;
+    const ComponentName = toPascalCase(iconName);
+    const iconNode = icons[ComponentName];
+    if (!iconNode) {
+      return console.warn(
+        `${element.outerHTML} icon name was not found in the provided icons object.`
+      );
+    }
+    const elementAttrs = getAttrs(element);
+    const [tag, iconAttributes, children] = iconNode;
+    const iconAttrs = {
+      ...iconAttributes,
+      "data-lucide": iconName,
+      ...attrs,
+      ...elementAttrs
+    };
+    const classNames = combineClassNames(["lucide", `lucide-${iconName}`, elementAttrs, attrs]);
+    if (classNames) {
+      Object.assign(iconAttrs, {
+        class: classNames
+      });
+    }
+    const svgElement = createElement$1([tag, iconAttrs, children]);
+    return element.parentNode?.replaceChild(svgElement, element);
+  };
+
+  // node_modules/lucide/dist/esm/defaultAttributes.js
+  var defaultAttributes = {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: 24,
+    height: 24,
+    viewBox: "0 0 24 24",
+    fill: "none",
+    stroke: "currentColor",
+    "stroke-width": 2,
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round"
+  };
+
+  // node_modules/lucide/dist/esm/icons/chevron-down.js
+  var ChevronDown = ["svg", defaultAttributes, [["path", { d: "m6 9 6 6 6-6" }]]];
+
+  // node_modules/lucide/dist/esm/icons/chevron-right.js
+  var ChevronRight = ["svg", defaultAttributes, [["path", { d: "m9 18 6-6-6-6" }]]];
+
+  // node_modules/lucide/dist/esm/icons/download.js
+  var Download = [
+    "svg",
+    defaultAttributes,
+    [
+      ["path", { d: "M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" }],
+      ["polyline", { points: "7 10 12 15 17 10" }],
+      ["line", { x1: "12", x2: "12", y1: "15", y2: "3" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/eye.js
+  var Eye = [
+    "svg",
+    defaultAttributes,
+    [
+      ["path", { d: "M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" }],
+      ["circle", { cx: "12", cy: "12", r: "3" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/file.js
+  var File = [
+    "svg",
+    defaultAttributes,
+    [
+      ["path", { d: "M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" }],
+      ["path", { d: "M14 2v4a2 2 0 0 0 2 2h4" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/flag.js
+  var Flag = [
+    "svg",
+    defaultAttributes,
+    [
+      ["path", { d: "M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" }],
+      ["line", { x1: "4", x2: "4", y1: "22", y2: "15" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/folder.js
+  var Folder = [
+    "svg",
+    defaultAttributes,
+    [
+      [
+        "path",
+        {
+          d: "M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z"
+        }
+      ]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/github.js
+  var Github = [
+    "svg",
+    defaultAttributes,
+    [
+      [
+        "path",
+        {
+          d: "M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+        }
+      ],
+      ["path", { d: "M9 18c-4.51 2-5-2-7-2" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/globe.js
+  var Globe = [
+    "svg",
+    defaultAttributes,
+    [
+      ["circle", { cx: "12", cy: "12", r: "10" }],
+      ["path", { d: "M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20" }],
+      ["path", { d: "M2 12h20" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/hash.js
+  var Hash = [
+    "svg",
+    defaultAttributes,
+    [
+      ["line", { x1: "4", x2: "20", y1: "9", y2: "9" }],
+      ["line", { x1: "4", x2: "20", y1: "15", y2: "15" }],
+      ["line", { x1: "10", x2: "8", y1: "3", y2: "21" }],
+      ["line", { x1: "16", x2: "14", y1: "3", y2: "21" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/layers.js
+  var Layers = [
+    "svg",
+    defaultAttributes,
+    [
+      [
+        "path",
+        {
+          d: "m12.83 2.18a2 2 0 0 0-1.66 0L2.6 6.08a1 1 0 0 0 0 1.83l8.58 3.91a2 2 0 0 0 1.66 0l8.58-3.9a1 1 0 0 0 0-1.83Z"
+        }
+      ],
+      ["path", { d: "m22 17.65-9.17 4.16a2 2 0 0 1-1.66 0L2 17.65" }],
+      ["path", { d: "m22 12.65-9.17 4.16a2 2 0 0 1-1.66 0L2 12.65" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/mail.js
+  var Mail = [
+    "svg",
+    defaultAttributes,
+    [
+      ["rect", { width: "20", height: "16", x: "2", y: "4", rx: "2" }],
+      ["path", { d: "m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/map.js
+  var Map = [
+    "svg",
+    defaultAttributes,
+    [
+      ["polygon", { points: "3 6 9 3 15 6 21 3 21 18 15 21 9 18 3 21" }],
+      ["line", { x1: "9", x2: "9", y1: "3", y2: "18" }],
+      ["line", { x1: "15", x2: "15", y1: "6", y2: "21" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/message-circle.js
+  var MessageCircle = [
+    "svg",
+    defaultAttributes,
+    [["path", { d: "M7.9 20A9 9 0 1 0 4 16.1L2 22Z" }]]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/search.js
+  var Search = [
+    "svg",
+    defaultAttributes,
+    [
+      ["circle", { cx: "11", cy: "11", r: "8" }],
+      ["path", { d: "m21 21-4.3-4.3" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/twitter.js
+  var Twitter = [
+    "svg",
+    defaultAttributes,
+    [
+      [
+        "path",
+        {
+          d: "M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"
+        }
+      ]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/user.js
+  var User = [
+    "svg",
+    defaultAttributes,
+    [
+      ["path", { d: "M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" }],
+      ["circle", { cx: "12", cy: "7", r: "4" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/users.js
+  var Users = [
+    "svg",
+    defaultAttributes,
+    [
+      ["path", { d: "M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" }],
+      ["circle", { cx: "9", cy: "7", r: "4" }],
+      ["path", { d: "M22 21v-2a4 4 0 0 0-3-3.87" }],
+      ["path", { d: "M16 3.13a4 4 0 0 1 0 7.75" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/wrench.js
+  var Wrench = [
+    "svg",
+    defaultAttributes,
+    [
+      [
+        "path",
+        {
+          d: "M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"
+        }
+      ]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/icons/x.js
+  var X = [
+    "svg",
+    defaultAttributes,
+    [
+      ["path", { d: "M18 6 6 18" }],
+      ["path", { d: "m6 6 12 12" }]
+    ]
+  ];
+
+  // node_modules/lucide/dist/esm/lucide.js
+  var createIcons = ({ icons = {}, nameAttr = "data-lucide", attrs = {} } = {}) => {
+    if (!Object.values(icons).length) {
+      throw new Error(
+        "Please provide an icons object.\nIf you want to use all the icons you can import it like:\n `import { createIcons, icons } from 'lucide';\nlucide.createIcons({icons});`"
+      );
+    }
+    if (typeof document === "undefined") {
+      throw new Error("`createIcons()` only works in a browser environment.");
+    }
+    const elementsToReplace = document.querySelectorAll(`[${nameAttr}]`);
+    Array.from(elementsToReplace).forEach(
+      (element) => replaceElement(element, { nameAttr, icons, attrs })
+    );
+    if (nameAttr === "data-lucide") {
+      const deprecatedElements = document.querySelectorAll("[icon-name]");
+      if (deprecatedElements.length > 0) {
+        console.warn(
+          "[Lucide] Some icons were found with the now deprecated icon-name attribute. These will still be replaced for backwards compatibility, but will no longer be supported in v1.0 and you should switch to data-lucide"
+        );
+        Array.from(deprecatedElements).forEach(
+          (element) => replaceElement(element, { nameAttr: "icon-name", icons, attrs })
+        );
+      }
+    }
+  };
+
+  // <stdin>
+  createIcons({
+    icons: {
+      Twitter,
+      X,
+      ChevronDown,
+      ChevronRight,
+      Github,
+      Hash,
+      Mail,
+      Folder,
+      File,
+      MessageCircle,
+      Search,
+      Download,
+      Wrench,
+      Users,
+      User,
+      Globe,
+      Eye,
+      Layers,
+      Flag,
+      Map
+    },
+    attrs: { "aria-hidden": true }
+  });
+})();
+/*! Bundled license information:
+
+lucide/dist/esm/createElement.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/replaceElement.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/defaultAttributes.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/chevron-down.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/chevron-right.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/download.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/eye.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/file.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/flag.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/folder.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/github.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/globe.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/hash.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/layers.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/mail.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/map.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/message-circle.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/search.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/twitter.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/user.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/users.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/wrench.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/icons/x.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+
+lucide/dist/esm/lucide.js:
+  (**
+   * @license lucide v0.344.0 - ISC
+   *
+   * This source code is licensed under the ISC license.
+   * See the LICENSE file in the root directory of this source tree.
+   *)
+*/
