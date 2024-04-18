fe9d8fa175a8d5333d74a273801611bf8399df65
Clément Biron <clement.biron@gmail.com>
2024-04-18 17:49:01 +0200

    Simplify code

--- test/cypress/e2e/i18n.cy.js
+++ test/cypress/e2e/i18n.cy.js
@@ -1,8 +1,6 @@
-const LANGUAGES = {
-  default: 'en',
-  supported: [ 'en', 'fr' ],
-  unsupported: ['jp'],
-};
+const DEFAULT_LANGUAGE = 'en';
+const OTHER_SUPPORTED_LANGUAGE = 'fr';
+const UNSUPPORTED_LANGUAGE = 'ja';
 
 const HOMEPAGE = {
   en: { permalink: `${Cypress.config().baseUrl}/en/` },
@@ -27,7 +25,7 @@ describe('i18n', () => {
         beforeEach(() => {
           cy.visit('/', {
             onBeforeLoad(window) {
-              Object.defineProperty(window.navigator, 'languages', { value: [LANGUAGES.supported[1]] });
+              Object.defineProperty(window.navigator, 'language', { value: OTHER_SUPPORTED_LANGUAGE });
             },
           });
         });
@@ -35,7 +33,7 @@ describe('i18n', () => {
           cy.location('href').should('equal', HOMEPAGE.fr.permalink);
         });
         it('defines the proper HTML lang attribute', () => {
-          cy.get('html').should('have.attr', 'lang', LANGUAGES.supported[1]);
+          cy.get('html').should('have.attr', 'lang', OTHER_SUPPORTED_LANGUAGE);
         });
         it('displays the content in the proper language', () => {
           cy.get('.footer').contains('Politique de confidentialité');
@@ -45,7 +43,7 @@ describe('i18n', () => {
         beforeEach(() => {
           cy.visit('/', {
             onBeforeLoad(window) {
-              Object.defineProperty(window.navigator, 'languages', { value: [LANGUAGES.unsupported[0]] });
+              Object.defineProperty(window.navigator, 'language', { value: UNSUPPORTED_LANGUAGE });
             },
           });
         });
@@ -53,7 +51,7 @@ describe('i18n', () => {
           cy.location('href').should('equal', HOMEPAGE.en.permalink);
         });
         it('defines the proper HTML lang attribute', () => {
-          cy.get('html').should('have.attr', 'lang', LANGUAGES.default);
+          cy.get('html').should('have.attr', 'lang', DEFAULT_LANGUAGE);
         });
         it('displays the content in the proper language', () => {
           cy.get('.footer').contains(PRIVACY_POLICY.en.text);
@@ -69,7 +67,7 @@ describe('i18n', () => {
           cy.location('href').should('equal', HOMEPAGE.fr.permalink);
         });
         it('defines the proper HTML lang attribute', () => {
-          cy.get('html').should('have.attr', 'lang', LANGUAGES.supported[1]);
+          cy.get('html').should('have.attr', 'lang', OTHER_SUPPORTED_LANGUAGE);
         });
         it('displays the content in the proper language', () => {
           cy.get('.footer').contains(PRIVACY_POLICY.fr.text);
@@ -83,7 +81,7 @@ describe('i18n', () => {
         beforeEach(() => {
           cy.visit('/privacy-policy', {
             onBeforeLoad(window) {
-              Object.defineProperty(window.navigator, 'languages', { value: [LANGUAGES.supported[1]] });
+              Object.defineProperty(window.navigator, 'language', { value: OTHER_SUPPORTED_LANGUAGE });
             },
           });
         });
@@ -91,7 +89,7 @@ describe('i18n', () => {
           cy.location('href').should('equal', PRIVACY_POLICY.fr.permalink);
         });
         it('defines the proper HTML lang attribute', () => {
-          cy.get('html').should('have.attr', 'lang', LANGUAGES.supported[1]);
+          cy.get('html').should('have.attr', 'lang', OTHER_SUPPORTED_LANGUAGE);
         });
         it('displays the content in the proper language', () => {
           cy.get('.footer').contains(PRIVACY_POLICY.fr.text);
