455c0133ad208f088efb9ceab6f2a8399fb536f3
Clément Biron <clement.biron@gmail.com>
2024-04-18 14:04:08 +0200

    Simplify test runner

--- test/cypress/e2e/index.js
+++ test/cypress/e2e/index.js
@@ -1,37 +1,32 @@
 const { spawn, execSync } = require('child_process');
-const waitOn = require('wait-on');
+
 const cypress = require('cypress');
+const waitOn = require('wait-on');
 
-console.log('→ Stop running Hugo');
-try {
-  execSync('pkill -f hugo').toString();
-} catch (err) {}
+console.log('→ Start Hugo if necessary');
+const hugoServer = spawn('hugo', [ 'serve', '--logLevel', 'error', '--port', '1313' ], { shell: true, detached: true });
 
-console.log('→ Start new Hugo');
-const hugoServer = spawn('hugo', ['serve', '--logLevel', 'error', '--port', '1313'], { shell: true, detached: true });
-
-hugoServer.stderr.on('data', (data) => {
-  console.error('Hugo error:', data.toString());
+hugoServer.stderr.on('data', data => {
+  console.error('Error when starting Hugo:', data.toString());
 });
 
-waitOn({ resources: ['http://localhost:1313'] }, async (err) => {
-    if (err) {
-      console.error("Error when waiting for Hugo", err);
-      return hugoServer.kill('SIGTERM');
-    }
+waitOn({ resources: ['http://localhost:1313'] }, async error => {
+  if (error) {
+    console.error('Error when waiting for Hugo', error);
 
-    console.log('→ Start Cypress');
+    return hugoServer.kill('SIGTERM');
+  }
+
+  console.log('→ Start Cypress');
+  try {
+    await cypress.run({ configFile: 'test/cypress/cypress.config.js' });
+  } catch (error) {
+    console.error('Cypress error', error);
+  } finally {
     try {
-      const results = await cypress.run({
-        configFile: 'test/cypress/cypress.config.js'
-      });
-    } catch (err) {
-      console.error('Cypress error', err);
-    } finally {
-      try {
-        hugoServer.kill('SIGTERM');
-      } catch (err) {
-        console.error('Error when stopping Hugo', err);
-      }
+      hugoServer.kill('SIGTERM');
+    } catch (error) {
+      console.error('Error occurred while trying to kill the Hugo process:', error);
     }
+  }
 });
