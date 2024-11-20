e40525b284da149311c47abc6c4a09c862529d4f
clementbiron <clementbiron@users.noreply.github.com>
2024-11-20 14:46:10 +0000

    deploy: 56064cf63f5d69dc7e8229b8e31f9b76a5bb3388

--- js/icons.js
+++ js/icons.js
@@ -187,21 +187,6 @@
     ]
   ];
 
-  // node_modules/lucide/dist/esm/icons/github.js
-  var Github = [
-    "svg",
-    defaultAttributes,
-    [
-      [
-        "path",
-        {
-          d: "M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
-        }
-      ],
-      ["path", { d: "M9 18c-4.51 2-5-2-7-2" }]
-    ]
-  ];
-
   // node_modules/lucide/dist/esm/icons/globe.js
   var Globe = [
     "svg",
@@ -433,7 +418,6 @@
       ChevronDown,
       ChevronRight,
       ChevronLeft,
-      Github,
       Hash,
       Mail,
       Folder,
@@ -578,14 +562,6 @@ lucide/dist/esm/icons/flag.js:
    *)
 
 lucide/dist/esm/icons/folder.js:
-  (**
-   * @license lucide v0.344.0 - ISC
-   *
-   * This source code is licensed under the ISC license.
-   * See the LICENSE file in the root directory of this source tree.
-   *)
-
-lucide/dist/esm/icons/github.js:
   (**
    * @license lucide v0.344.0 - ISC
    *
